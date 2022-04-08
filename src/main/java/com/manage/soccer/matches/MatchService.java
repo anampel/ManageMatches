b981bebe9ffcd4dc0b83757d1e0609ad70de8c53
anampel <anampel.am@gmail.com>
2022-04-08 14:40:40 +0300

    addNewMatch version 2

--- src/main/java/com/manage/soccer/matches/MatchService.java
+++ src/main/java/com/manage/soccer/matches/MatchService.java
@@ -3,45 +3,46 @@ package com.manage.soccer.matches;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 @Service
 public class MatchService {
     private MatchRepository matchRepository;
-
+    private static final String timeFormat ="HH:mm:ss";
     @Autowired
     public MatchService(MatchRepository matchRepository) {
         this.matchRepository = matchRepository;
     }
 
-    public Optional<Match> findMatchById(Long id){
-       return matchRepository.findMatchById(id);
+    public Optional<Match> findMatchById(Long id) {
+        return matchRepository.findMatchByMatch_id(id);
     }
 
     public void addNewMatch(Match match){
-//        Optional<Match> matchById = matchRepository.findMatchById();
-//        if (productByName.isPresent()) {
-//            throw new IllegalStateException("The product already exists!!");
-//        }
-
-        matchRepository.save(match);
+        try {
+            match.setMatch_time(new SimpleDateFormat(timeFormat).parse("15:30:14"));
+            matchRepository.save(match);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    public void editMatch(Match match){
-        Optional<Match> matchById = matchRepository.findMatchById(match.getId());
+    public void editMatch(Match match) {
+        Optional<Match> matchById = matchRepository.findMatchByMatch_id(match.getId());
         if (matchById.isPresent()) {
             matchRepository.save(match);
-        }else{
+        } else {
             throw new IllegalArgumentException("The match_id: " + match.getId() + " does not exist!");
         }
     }
 
-    public void deleteMatch(Long id){
-        Optional<Match> matchById = matchRepository.findMatchById(id);
+    public void deleteMatch(Long id) {
+        Optional<Match> matchById = matchRepository.findMatchByMatch_id(id);
         if (matchById.isPresent()) {
             matchRepository.deleteById(id);
-        }else{
+        } else {
             throw new IllegalArgumentException("The match_id: " + id + " does not exist!");
         }
     }
